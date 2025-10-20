36dd1d25c4a1f2ff8f64c94fad28e9cdff241c05
Celebration-cloud <ojinguluc@gmail.com>
2025-10-20 10:25:53 +0100

    image domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "cdn.jsdelivr.net",
+      "upload.wikimedia.org",
+      "cdn-icons-png.flaticon.com",
+      "raw.githubusercontent.com",
+    ],
+  },
+};
 
 module.exports = nextConfig;
